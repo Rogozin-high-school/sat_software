cb5a6aba6f1e5e6a961ecac78bb945e038bdcb3b
mgmaor <mgmaorvids@gmail.com>
2019-09-05 17:13:02 +0300

    Added magnetometer

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,35 @@
 
 #include "../include/logger.h"
 #include "../include/client.h"
+#include "../modules/magnetometer/magnetometer.h"
 
 int main(void) {
     Satellite::Logger::log("Starting the satellite!");
 
+    // Start the client
     Satellite::Client client;
     if (client.failure()) {
         Satellite::Logger::log("Connection has failed!");
-        return 1;
+        return -1;
     }
-    Satellite::Logger::log("Connected!");
+    Satellite::Logger::log("Client connected!");
+
+    // Start the magnetometer
+    Satellite::Modules::Magnetometer magnetometer;
+    int code = magnetometer.get_error_code();
+    switch (code) {
+    case MPUIMU::ERROR_IMU_ID:
+        Satellite::Logger::log("Bad IMU device ID!");
+        return code;
+    case MPUIMU::ERROR_MAG_ID:
+        Satellite::Logger::log("Bad magnetometer device ID!");
+        return code;
+    case MPUIMU::ERROR_SELFTEST:
+        Satellite::Logger::log("Failed self test!");
+        return code;
+    default:
+        Satellite::Logger::log("MPU9250 online!");
+    }
     
     return 0;
 }
